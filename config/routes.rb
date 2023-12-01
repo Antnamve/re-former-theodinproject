98de0b64c31a4ceb36f450a0d8699a1e8732343b
Antnamve <antnamve@gmail.com>
2023-12-01 20:18:17 +0300

    finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get 'users/new'
+  get 'users/create'
+  resources :users, except: %i[destroy]
 end
